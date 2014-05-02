6d80b2eaadc3d7509f1aab57e4158d73347e8767
John Peloquin <john.peloquin@gmail.com>
2014-05-02 14:01:08 -0400

    Remove broken test for hdf5ify

--- test/test_vic2d.py
+++ test/test_vic2d.py
@@ -8,7 +8,3 @@ fp = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                   "fixtures",
                   "cam0_038228_5737.937.csv")
 # fig = plot_strains(fp, "out.png")
-
-def test_hdf5ify():
-    csvfiles = [fp]
-    hdf5ify(csvfiles)
