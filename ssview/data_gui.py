58a9a88ed9ceb4be5b0ac252b37629ca1f2da788
John Peloquin <john.peloquin@gmail.com>
2014-05-13 17:21:28 -0400

    Revert "Antialias plots and use wider lines"
    This reverts commit eb5b097208470d3b5b1f7a72e3a9cf630b50eea9.
    
    It turns out that using an explicitly defined pen slowed down
    rendering enough to make the interface laggy.

--- ssview/data_gui.py
+++ ssview/data_gui.py
@@ -87,19 +87,12 @@ class DataView(QtGui.QWidget):
     def load_data(self, fpath):
         self.data = TestData(fpath)
         # Populate line plots with data
-        pen = pg.mkPen(color="FFF", width=2)
         self.stretch_vs_time.plot(x=self.data.time,
-                                  y=self.data.stretch,
-                                  antialias=True,
-                                  pen=pen)
+                                   y=self.data.stretch)
         self.stress_vs_time.plot(x=self.data.time,
-                                 y=self.data.stress,
-                                 antialias=True,
-                                 pen=pen)
+                                   y=self.data.stress)
         self.stress_vs_stretch.plot(x=self.data.stretch,
-                                    y=self.data.stress,
-                                    antialias=True,
-                                    pen=pen)
+                                   y=self.data.stress)
         self.stretch_vs_time.marker.setPos(self.data.time[0])
         self.stress_vs_time.marker.setPos(self.data.time[0])
         self.stress_vs_stretch.marker.setPos(self.data.stretch[0])
