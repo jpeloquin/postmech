27aad06eba9fe9d266346c5062383fa4433c0a6f
John Peloquin <john.peloquin@gmail.com>
2014-05-14 15:27:17 -0400

    ssview: Set colormap scale using quantiles, fix +/- inversion
    The quantiles are calculated for each strain field individually,
    then the maximum of absolute value is chosen for each of exx,
    eyy, and ezz and used for the entire dataset.
    
    The positive and negative strains were inverted when makeARGB was
    applied; this is because pyqtgraph expects RGBA images.  Note that
    makeARGB actually stores data in BGRA order (this is a Qt quirk).

--- ssview/datainput.py
+++ ssview/datainput.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import mechana
 import matplotlib.image as mpimg
+import pyqtgraph as pg
 
 def debug_trace():
     """Set a tracepoint in the Python debugger that works with Qt.
@@ -12,6 +13,35 @@ def debug_trace():
     from pdb import set_trace
     pyqtRemoveInputHook()
     set_trace()
+
+cmap_div = pg.ColorMap(pos=np.arange(0.0, 1.0, 0.1),
+                       color=np.array([[0.0941, 0.3098, 0.6353, 1.0],
+                                       [0.2745, 0.3882, 0.6824, 1.0],
+                                       [0.4275, 0.6000, 0.8078, 1.0],
+                                       [0.6275, 0.7451, 0.8824, 1.0],
+                                       [0.8118, 0.8863, 0.9412, 1.0],
+                                       [0.9451, 0.9569, 0.9608, 1.0],
+                                       [0.9569, 0.8549, 0.7843, 1.0],
+                                       [0.9725, 0.7216, 0.5451, 1.0],
+                                       [0.8824, 0.5725, 0.2549, 1.0],
+                                       [0.7333, 0.4706, 0.2118, 1.0],
+                                       [0.5647, 0.3922, 0.1725, 1.0]]))
+cmap_div_lut = cmap_div.getLookupTable()
+
+def render_image(img, levels=None):
+    isnan = np.isnan(img)
+    extrema = np.percentile(img[~np.isnan(img)], (5, 95))
+    absmax = np.abs(np.max(img))
+    if absmax == 0.0:
+        absmax = 1.0
+    if levels is None:
+        extremum = np.max(np.abs(extrema))
+        levels = [-extremum, extremum]
+    img_argb = np.nan_to_num(img)
+    img_argb, b = pg.makeRGBA(np.nan_to_num(img),
+                              levels=levels, lut=cmap_div_lut)
+    img_argb[isnan] = 0
+    return img_argb
 
 class TestData:
     """Test data (mechanical data, images, strain fields).
@@ -25,7 +55,8 @@ class TestData:
     imagelist = None
     image_t0 = None
 
-    strainfields = None # currently list of vic2d csv files; may change
+    strainfields = None # list of monochrome uint8 images
+    strainfields_argb = None # list of rgba images
     fieldtimes = None
 
     def __init__(self, jsonfile):
@@ -82,8 +113,29 @@ class TestData:
                           'exy': exy}
                 return fields
 
-            fields = [get_fields(df) for df in dfs]
-            self.strainfields = fields
+            fieldlist = [get_fields(df) for df in dfs]
+            self.strainfields = fieldlist
+
+            # Get overall quantiles for each strain field
+            fnames = ['exx', 'eyy', 'exy']
+            extrema = {}
+            for fn in fnames:
+                ims = (fd[fn] for fd in self.strainfields)
+                ims = (im[~np.isnan(im)] for im in ims)
+                l = (max(np.abs(np.percentile(im, (5, 95))))
+                     for im in ims)
+                extrema[fn] = max(l)
+
+            def render_field_dict(fields, extrema):
+                fields_rgba = dict()
+                for k in fields:
+                    levels = (-extrema[k], extrema[k])
+                    fields_rgba[k] = render_image(fields[k],
+                                                  levels=levels)
+                return fields_rgba
+
+            self.strainfields_argb = [render_field_dict(fd, extrema)
+                                      for fd in fieldlist]
 
             csvnames = (os.path.basename(f) for f in vic2dfiles)
             fieldtimes = [mechana.images.image_time(nm)
@@ -109,7 +161,8 @@ class TestData:
       idx = np.argmin(np.abs(self.fieldtimes - t))
       fieldtime = self.fieldtimes[idx]
       fields = self.strainfields[idx]
-      return fields, fieldtime
+      fields_argb = self.strainfields_argb[idx]
+      return fields, fields_argb, fieldtime
 
     def image_at(self, t):
         """Return image at time t.
