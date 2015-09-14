c9d43219f438ac8e6f6a6f094b62f84634e3ccf1
John Peloquin <john.peloquin@gmail.com>
2015-09-14 17:56:52 -0400

    Fix mechana.images tests

--- test/test_images.py
+++ test/test_images.py
@@ -4,7 +4,7 @@ def test_decode_impath_tiff():
     pth = 'cam0_12345_10.1.tiff'
     expected = {'camera id': '0',
                 'frame id': '12345',
-                'timestamp': 10.1}
+                'timestamp (s)': 10.1}
     actual = decode_impath(pth)
     for k in expected:
         assert actual[k] == expected[k]
@@ -13,7 +13,7 @@ def test_decode_impath_csv():
     pth = 'cam0_12345_10.1.csv'
     expected = {'camera id': '0',
                 'frame id': '12345',
-                'timestamp': 10.1}
+                'timestamp (s)': 10.1}
     actual = decode_impath(pth)
     for k in expected:
         assert actual[k] == expected[k]
