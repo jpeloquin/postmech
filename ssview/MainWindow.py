c6186535fcd40e62fc044214b0bc2db99841116c
John Peloquin <john.peloquin@gmail.com>
2015-09-14 00:00:44 -0400

    Delete unused & redundant functions

--- ssview/MainWindow.py
+++ ssview/MainWindow.py
@@ -47,39 +47,6 @@ class MainWindow(QtGui.QMainWindow):
         self.currentfile = pth
         self.data_view.load_data(pth)
 
-    def load_images(self, imdir):
-        """Load images into memory and find time of each frame.
-
-        The times are calculated to be consistent with the stress &
-        strain data file.  The reference image (typically defined as
-        the frame of first motion) is assigned the time given in
-        `ref_time.csv`; the remaining frame times are calculated from
-        their file names.
-
-        """
-        # Load image names and times
-        imlist = []
-        for fname in os.listdir(imdir):
-            if fname.endswith((".tiff", ".tif")):
-                imlist.append(fname)
-        # Load reference time
-        fpath_reftime = os.path.join(imdir, "ref_time.csv")
-        with open(fpath_reftime, 'rb') as f:
-            reader = csv.reader(f)
-            physical_reftime = float(reader.next()[0])
-        # Apply offset such that reference frame has reference time
-        fpath_imindex = os.path.join(imdir, "image_index.csv")
-        refimgname = testdataIO.imindex_lookup(fpath_imindex, "reference")
-        imtime = np.array(testdataIO.imtime(imlist))
-        image_reftime = imtime[imlist.index(refimgname)]
-        imtime = imtime - image_reftime + physical_reftime
-        # Load images into memory
-        images = []
-        for fname in imlist:
-            fpath = os.path.join(imdir, fname)
-            images.append(np.array(Image.open(fpath)).T)
-        return sorted(zip(imtime, images))
-
 def main(pth_data=None):
     win = MainWindow()
     win.show()
