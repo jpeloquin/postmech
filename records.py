f3389173d985da6ef778562746e631bd83b7b437
John Peloquin <john.peloquin@gmail.com>
2015-09-14 00:05:03 -0400

    Add class for locating test data files
    This should reduce the amount of file path manipulation necessary to
    iterate over tests listed in notch_testing.specimens.specimens.

---
+++ records.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+
+import pandas as pd
+
+from .images import list_images
+
+class Test:
+    """Find and store paths of test data files from test records.
+
+    """
+
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
+        self.test_dir = None
+        self.stress_strain_file = None
+        self.image_dir = None
+        self.image_paths = []
+        self.vic2d_dir = None
+        self.vic2d_paths = []
+
+    @classmethod
+    def from_row(cls, project_dir, row):
+        self = cls(project_dir)
+        ## Primary data folder for test
+        self.test_dir = os.path.join(project_dir, 'data',
+                                     row['ramp to failure folder'])
+
+        ## Image list
+        if not pd.isnull(row['image directory']):
+            self.image_dir = os.path.join(project_dir, 'data',
+                                          row['image directory'])
+            self.image_paths = list_images(self.image_dir)
+
+        ## Vic-2d directory
+        if not pd.isnull(row['vic-2d export folder']):
+            self.vic2d_dir = os.path.join(project_dir,
+                                          row['vic-2d export folder'])
+
+        ## Stress and strain data file
+        self.stress_strain_file = os.path.join(self.test_dir,
+                                               'stress_strain.csv')
+        if not os.path.exists(self.stress_strain_file):
+            self.stress_strain_file = None
+            print("Warning: {} test {} has no stress_strain.csv "
+                  "file.".format(row['specimen id'], row['test id']))
+
+        return self
