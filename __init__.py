e51121458c2a262e78fc868a8a43371c84b06881
John Peloquin <john.peloquin@gmail.com>
2015-11-13 11:00:59 -0500

    Add mechana.records to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-import images
 import analysis
-import vic2d
-import read
+import images
 import instron
+import read
+import records
 import unit
+import vic2d
 import write
 from organize import write_test_file
