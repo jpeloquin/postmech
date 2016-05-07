1412201570754d641bef1497fd951a836a3a29b5
John Peloquin <john.peloquin@gmail.com>
2016-05-06 23:16:44 -0400

    Add option for # units in write.measurement_csv

--- write.py
+++ write.py
@@ -3,7 +3,7 @@ import os
 
 from .unit import ureg
 
-def measurement_csv(m, fpath):
+def measurement_csv(m, fpath, digits=7):
     """Write measurement to csv file.
 
     m := pint value with units and uncertainty
@@ -12,6 +12,8 @@ def measurement_csv(m, fpath):
     with open(fpath, 'w', newline='') as  f:
         csvwriter = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC,
                                lineterminator=os.linesep)
-        csvwriter.writerow([m.value.magnitude,
-                            m.error.magnitude,
-                            str(m.units)])
+        values = [m.value.magnitude, m.error.magnitude]
+        units = str(m.units)
+        num_format = "{:." + str(digits) + "g}"
+        values = [num_format.format(x) for x in values]
+        csvwriter.writerow(values + [units])
