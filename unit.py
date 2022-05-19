66c54be9f6303f87ab30f346e578a7543d41ff31
John Peloquin <john.peloquin@gmail.com>
2022-05-19 15:55:45 -0400

    Add function to parse "a ± b units" string

--- unit.py
+++ unit.py
@@ -1,4 +1,21 @@
+import re
+
 from pint import UnitRegistry
+from uncertainties import ufloat
 
 ureg = UnitRegistry()
 ureg.define("pixel = [distance] = px")
+
+
+def parse(s: str) -> ureg.Quantity:
+    """Parse a value with uncertainty and units, e.g. '1 ± 0.5 mm'"""
+    m = re.match(
+        r"(?P<value>[0-9.]+)\s*(?:±|\+/-)\s*(?P<uncertainty>[0-9.]+)?\s*(?P<units>\S+)?",
+        s,
+    )
+    if m.group("uncertainty") is None:
+        return float(m.group("value")) * ureg(m.group("units"))
+    else:
+        return ufloat(float(m.group("value")), float(m.group("uncertainty"))) * ureg(
+            m.group("units")
+        )
