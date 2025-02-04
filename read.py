1059fe06ea9782153637fb65228ffd9ecf499eb6
John Peloquin <john.peloquin@gmail.com>
2025-02-03 22:04:47 -0500

    Fix metadata line test in instron_rawdata

--- read.py
+++ read.py
@@ -245,7 +245,7 @@ def instron_rawdata(fpath, thousands_sep=","):
 
         """
         split = ln[0].split(" : ")
-        if len(split) == 2 and len(split[0] > 0) and len(split[1] > 0):
+        if len(split) == 2 and len(split[0]) > 0 and len(split[1]) > 0:
             return True
         return False
 
@@ -270,7 +270,6 @@ def instron_rawdata(fpath, thousands_sep=","):
                     v = ln[1]
                     if len(ln) > 2:
                         unit = ln[2].strip()
-                        breakpoint()
                         v = float(strip_sep(v, thousands_sep)) * ureg(unit)
                     metadata[k] = v
             except StopIteration:
